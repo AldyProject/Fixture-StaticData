cca569ef8db3f01a741d7b614caf3d42f5019108
AldyProject <aldysuryanto001@gmail.com>
2024-09-27 13:31:25 +0700

    Upload New File

---
+++ cypress.config.js
@@ -0,0 +1,14 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+ e2e: {
+ setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+},
+});
+
+module.exports = {
+  pageLoadTimeout: 120000, // Set to 120 seconds
+  // other configurations...
+};
